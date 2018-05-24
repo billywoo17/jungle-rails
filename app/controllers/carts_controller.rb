5d41a5ef70b87ce45d5b31ef3edc6b7ceae9c4c6
billywoo17 <billwu_@outlook.com>
2018-05-23 21:38:44 -0700

    fixed sold out

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,6 +4,9 @@ class CartsController < ApplicationController
   end
 
   def add_item
+    
+    @quantity = params[:product_quantity].to_f
+    if ( @quantity > 0 )
     product_id = params[:product_id].to_s
 
     item = cart[product_id] || { "quantity" => 0 }
@@ -12,6 +15,9 @@ class CartsController < ApplicationController
     update_cart cart
 
     redirect_to :back
+    else
+      redirect_to :back
+    end
   end
 
   def remove_item
