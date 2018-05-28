22a2ea0c0d1d9618dffa1b5b9a29447285166f83
bill <billwu_@outlook.com>
2018-05-28 23:29:21 +0000

    finished testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,26 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  before_validation :strip_whitespace
+  validates :password, presence: true, length: {minimum: 3}
+  validates :password_confirmation, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, 
+                    :uniqueness => {:case_sensitive => false}
 
 
-end
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.downcase.strip)
+    if user && user.authenticate(password)
+      user
+    else 
+      nil
+    end
+  end
+
+  def strip_whitespace
+    self.email = self.email.strip unless self.email.nil?
+    end
+
+  end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,13 +25,57 @@ RSpec.describe User, type: :model do
   expect(user).to_not be_valid
   end
 
+  it "password should be 3 letters or longer" do
+    user = User.new({
+      email: "email@fam.jcole",
+      password: "12",
+      password_confirmation: "12",
+      first_name: "Jermaine",
+      last_name: "Cole",
+    })
+  user.save    
+  expect(user).to_not be_valid
+  end
+
   it "email must be unique" do
     user = User.new({
       email: "email@fam.jcole",
       password: "12313123",
-      password_confirmation: "not the same",
+      password_confirmation: "12313123",
       first_name: "Jermaine",
       last_name: "Cole",
+    })  
+    user.save
+
+    user2 = User.new({
+      email: "EmAil@fam.jcole",
+      password: "123456",
+      password_confirmation: "123456",
+      first_name: "James",
+      last_name: "Smith",
     }) 
-    user.save  
+    user2.save
+    expect(user2).to_not be_valid
+  end
+
+  it "remove whitespace in email" do
+    user = User.new({
+      email: "email@fam.jcole",
+      password: "12313123",
+      password_confirmation: "12313123",
+      first_name: "Jermaine",
+      last_name: "Cole",
+    })  
+    user.save
+
+    user2 = User.new({
+      email: "email@fam.jcole  ",
+      password: "123456",
+      password_confirmation: "123456",
+      first_name: "James",
+      last_name: "Smith",
+    }) 
+    user2.save
+    expect(user2).to_not be_valid
+  end
 end
