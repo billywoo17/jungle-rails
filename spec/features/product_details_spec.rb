c1cdacbfd027ac8df01980f7de84abf5356ad764
billywoo17 <billwu_@outlook.com>
2018-05-29 15:44:51 -0700

    working product detail check

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+ # SETUP
+ before :each do
+  @category = Category.create! name: 'Apparel'
+
+  10.times do |n|
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 1,
+      price: 64.99
+    )
+    
+  end
+end
+
+scenario "They see all products" do
+  visit root_path
+  within first('article.product') do
+    click_link 'Detail'
+  end
+  expect(page).to have_css "article.product-detail"
+  end
 end
